ca37b28a96e7b2b9f7d7be27a8ac6f83ca748ab8
Diego Gutman <dgutman@lyracons.com>
2020-07-20 20:00:56 -0300

    agrego comando para actualizar la version de la consola

---
+++ src/Console/Command/Update.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace Lyratool\Console\Command;
+
+
+use Lyratool\Exception\ExecException;
+use Lyratool\Process\Exec;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Update extends Command
+{
+    /**
+     * @var string the name of the command
+     */
+    protected static $defaultName = 'update';
+
+    /**
+     * @var Exec
+     */
+    private $exec;
+
+    public function __construct(Exec $exec, string $name = null)
+    {
+        $this->exec = $exec;
+        
+        parent::__construct($name);
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Actualiza a la última versión de la aplicación')
+            ->setHelp('Actualiza a la última versión de la aplicación')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            $output->writeln("<info>Actualizando aplicación</info>");
+            $result = $this->exec->run(["docker", "pull", "dgutman/lyradock"], null, null, null, 60, true);
+            $output->writeln($result);
+
+            return Command::SUCCESS;
+        } catch (ExecException $exception) {
+            $output->writeln("<error>{$exception->getMessage()}</error>");
+
+            return Command::FAILURE;
+        }
+    }
+}
--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -8,6 +8,7 @@ use Exception;
 use Illuminate\Container\Container;
 use Lyratool\Console\Command\Docker\Compose\Down;
 use Lyratool\Console\Command\Docker\Compose\Up;
+use Lyratool\Console\Command\Update;
 use Lyratool\Exception\KernelException;
 use Symfony\Component\Console\Application as Console;
 
@@ -19,6 +20,8 @@ class Kernel
     private $console;
 
     private $commands = [
+        #Console
+        Update::class,
         # Docker
         Up::class,
         Down::class
